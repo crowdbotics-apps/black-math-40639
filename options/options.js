4050ccfdb4665b14fbd758845accc13dbda3a9da
Crowdbotics <team@crowdbotics.com>
2023-05-12 04:39:45 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "black-math-40639",
+  url: "https://black-math-40639.botics.co",
+  api: "https://black-math-40639.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
